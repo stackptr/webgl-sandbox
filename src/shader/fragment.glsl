3d6c94063b1eeea3427f057af45c5a0a0a347fde
✿ corey (they/them) <c@rey.foo>
2025-06-03 18:05:17 -0700

    Reorder and slow down

--- src/shader/fragment.glsl
+++ src/shader/fragment.glsl
@@ -4,8 +4,8 @@ uniform vec2 u_resolution;
 uniform float u_time;
 
 void main() {
-  const float freq = 6.28;
+  const float freq = 1.5;
   vec2 uv = gl_FragCoord.xy / u_resolution;
-  float t = 0.5 + 0.5 * sin(u_time + uv.x * freq); // oscillate between 0 and 1
+  float t = 0.5 + 0.5 * sin((uv.x + u_time) * freq); // oscillate between 0 and 1
   gl_FragColor = vec4(t, 0.0, 1.0 - t, 1.0); // gradient based on X
 }
