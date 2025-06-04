7f46c213ca66d0647e50412bfa6833c16fe55cc0
✿ corey (they/them) <c@rey.foo>
2025-06-03 18:16:02 -0700

    Pleasing colors

--- src/shader/fragment.glsl
+++ src/shader/fragment.glsl
@@ -5,7 +5,14 @@ uniform float u_time;
 
 void main() {
   const float freq = 1.5;
+
   vec2 uv = gl_FragCoord.xy / u_resolution;
+
   float t = 0.5 + 0.5 * sin((uv.x + u_time) * freq); // oscillate between 0 and 1
-  gl_FragColor = vec4(t, 0.0, 1.0 - t, 1.0); // gradient based on X
+  
+  vec3 color_1 = vec3(0.7, 0.1, 0.4); // mulberry
+  vec3 color_2 = vec3(0.9, 0.6, 0.1); // marigold
+  vec3 color = mix(color_1, color_2, t);
+
+  gl_FragColor = vec4(color, 1.0); // gradient based on X
 }
